3f1b02900cbe08fb43e7f4de18d411945693f0e2
Insuxed <javiersetiadi@gmail.com>
2025-12-09 19:22:27 +0700

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,8 +21,6 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        dd("AuthController used");
-
         $request->validate([
             'name' => 'required|string|max:255|unique:users,name',
             'email' => 'required|string|email|max:255|unique:users,email',
